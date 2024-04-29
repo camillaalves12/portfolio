1b64570c3ad42b83b99be662585ad5b9f6aff638
camilla <camillaalves285@gmail.com>
2024-04-29 16:13:14 -0300

    Adicionei o componente de Techs

---
+++ src/components/Techs/Techs.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import S from './styles.module.scss'
+
+export const Techs = ({ title, techs }) => {
+  return (
+    <div className={S.divTech}>
+      <h2> {title} </h2>
+      <div className={S.divTechs}>
+        {techs.map((tech, index) => (
+          <div key={index} className={S.techs}>
+            {tech.icon}
+            <p> {tech.name} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
