9ab38c25d9fb5d7838248f43efdc26c826bbf76c
camilla <camillaalves285@gmail.com>
2024-05-09 10:11:37 -0300

    Configurei corretamente o arquivo de main

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './app.css'
 
 import { Home } from './pages/Home/Home'
-import { Techs } from './components/Techs/Techs'
 import { About } from './components/About/About'
 import { PageProjects } from './pages/PageProjects/PageProjects'
 import { PageTechs } from './pages/PageTechs/PageTechs'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<PageProjects />} />
-        <Route path="/tech" element={<PageTechs />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
