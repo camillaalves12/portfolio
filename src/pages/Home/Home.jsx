98314fb1e1972d9a52ad7da981b1b1461a0f95f6
camilla <camillaalves285@gmail.com>
2024-05-13 08:50:36 -0300

    Removi as informações sobre mim.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,35 +8,25 @@ export const Home = () => {
       <div className={S.container}>
         <div className={S.itens}>
           <div className={S.itensText}>
-            <h5>Oi, eu sou a</h5>
-            <h1>Camilla Alves</h1>
+            <div>
+              <h5 className={S.iAm}>Oi, eu sou a</h5>
+              <h1 className={S.name}>Camilla Alves</h1>
+            </div>
             <h5 className={S.colorDev}> &gt; Front-end developer</h5>
-            <h6 className={S.description}>
-              Sou graduanda em Sistemas de Informação pela Universidade Federal
-              de Alagoas e uma desenvolvedora front-end dedicada. Possuo uma
-              base sólida em JavaScript, ReactJs, React Native, HTML, CSS,
-              Bootstrap e Sass,  estou constantemente
-              buscando expandir meus conhecimentos e habilidades.
-            </h6>
 
-            <p>
-              <span className={S.colorConst}>const</span>
-              <span className={S.colorName}> githubLink</span> ={' '}
-              <a href="https://github.com/camillaalves12" target="_blank" rel="noreferrer">
-                "https://github.com/camillaalves12"
-              </a>
-            </p>
+            <div className={S.divGit_Link}>
+              <p>
+                <span className={S.colorConst}>const</span>{' '}
+                <span className={S.colorName}>linkedinLink</span> ={' '}
+                <a href="https://www.linkedin.com/in/camilla-alves-/" target="_blank" rel="noreferrer">"https://linkedin.com/camillaalves"</a>
+              </p>
 
-            <p>
-              <span className={S.colorConst}>const</span>{' '}
-              <span className={S.colorName}>linkedinLink</span> ={' '}
-              <a
-                href="https://www.linkedin.com/in/camilla-alves-/"
-                target="_blank" rel="noreferrer"
-              >
-                "https://www.linkedin.com/in/camilla-alves-/"
-              </a>
-            </p>
+              <p>
+                <span className={S.colorConst}>const</span>{' '}
+                <span className={S.colorName}> githubLink</span> ={' '}
+                <a href="https://github.com/camillaalves12" target="_blank" rel="noreferrer">"https://github.com/camillaalves"</a>
+                </p>
+            </div>
           </div>
           <img src="../src/imgs/camilla 1.jpg" alt="Foto Camilla" />
         </div>
@@ -44,3 +34,14 @@ export const Home = () => {
     </>
   )
 }
+
+
+{/* <h6 className={S.description}>
+              Sou graduanda em Sistemas de Informação pela Universidade Federal
+              de Alagoas e uma desenvolvedora front-end dedicada. Possuo uma
+              base sólida em JavaScript, ReactJs, React Native, HTML, CSS,
+              Bootstrap e Sass,  estou constantemente
+              buscando expandir meus conhecimentos e habilidades.
+            </h6> */}
+
+            // 
