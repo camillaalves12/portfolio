53ddde608f17a483f338fcb8dbc4e750c8dd68b9
camilla <camillaalves285@gmail.com>
2024-05-24 08:44:07 -0300

    Adicionei o icon de menu ao nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import S from './styles.module.scss'
+import { AiOutlineMenu } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
@@ -9,16 +10,19 @@ export const Header = () => {
     <Navbar expand="lg">
       <Container className={S.header}>
         <NavLink to="/">camilla_alves</NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+          <AiOutlineMenu color="#cdcdcd" size={32} className={S.iconHam}/> {/* Substitua #cdcdcd pela cor desejada e 32 pelo tamanho desejado */}
+        </Navbar.Toggle>
+ 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/projects" className="nav-link" ClassName={S.active}>
+            <NavLink to="/projects" className={S.nav}>
               _projetos
             </NavLink>
-            <NavLink to="/tech" className="nav-link" ClassName={S.active}>
+            <NavLink to="/tech" className={S.nav}>
               _tecnologias
             </NavLink>
-            <NavLink to="/about" className="nav-link" ClassName={S.active}>
+            <NavLink to="/about" className={S.nav}>
               _sobre_mim
             </NavLink>
           </Nav>
