bdb350cc8b7a4d93e13d26ddb1d46d33a02144e3
camilla <camillaalves285@gmail.com>
2024-06-11 15:37:45 -0300

    Adicionei a função para modificar o tamanho dos icons

--- src/pages/PageTechs/PageTechs.jsx
+++ src/pages/PageTechs/PageTechs.jsx
@@ -11,26 +11,32 @@ import { DiBootstrap } from 'react-icons/di'
 import { FaGitAlt } from 'react-icons/fa6'
 import { TbBrandVite } from "react-icons/tb";
 import S from './styles.module.scss'
+import { useWindowSizeIcons } from '../../components/useWindowSizeIcons/useWindowSizeIcons'
 
-const techData = [
-  { icon: <IoLogoJavascript size={55} />, name: 'Javascript' },
-  { icon: <IoLogoHtml5 size={55} />, name: 'Html5' },
-  { icon: <IoLogoCss3 size={55} />, name: 'Css3' },
-  { icon: <IoLogoReact size={55} />, name: 'React' },
-  { icon: <IoLogoReact size={55} />, name: 'React Native' },
-  { icon: <TbBrandVite size={55} />, name: 'Vite' },
-  { icon: <DiBootstrap size={55} />, name: 'Bootstrap' },
-  { icon: <IoLogoSass size={55} />, name: 'Sass' },
-  { icon: <FaGitAlt size={55} />, name: 'Git' },
-]
 
 export const PageTechs = () => {
+  
+  const { width } = useWindowSizeIcons();
+  const iconSize = width > 768 ? 55 : 30; // Ajuste os valores conforme necessário
+  
+  const techData = [
+    { icon: <IoLogoJavascript size={iconSize} />, name: 'Javascript' },
+    { icon: <IoLogoHtml5 size={iconSize} />, name: 'Html5' },
+    { icon: <IoLogoCss3 size={iconSize} />, name: 'Css3' },
+    { icon: <IoLogoReact size={iconSize} />, name: 'React' },
+    { icon: <IoLogoReact size={iconSize} />, name: 'React Native' },
+    { icon: <TbBrandVite size={iconSize} />, name: 'Vite' },
+    { icon: <DiBootstrap size={iconSize} />, name: 'Bootstrap' },
+    { icon: <IoLogoSass size={iconSize} />, name: 'Sass' },
+    { icon: <FaGitAlt size={iconSize} />, name: 'Git' },
+  ]
+
   return (
     <>
-          <div className={S.container}>
+      <div className={S.container}>
         <Techs title="Linguagens" techs={techData.slice(0, 3)} />
         <Techs title="Tecnologias" techs={techData.slice(3)} />
-      </div>
+    </div>
     </>
   )
 }
