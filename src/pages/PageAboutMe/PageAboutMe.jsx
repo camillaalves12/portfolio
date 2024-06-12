966db9e0a56af81f12e19047cbfdbe188f1b811b
camilla <camillaalves285@gmail.com>
2024-06-11 21:16:02 -0300

    retirei o loop de aparição

--- src/pages/PageAboutMe/PageAboutMe.jsx
+++ src/pages/PageAboutMe/PageAboutMe.jsx
@@ -10,7 +10,7 @@ export const PageAboutMe = () => {
       t.innerHTML = t.textContent.replace(/./g, "<span class='letra'>$&</span>");
     });
 
-    anime.timeline({loop: true})
+    anime.timeline()
       .add({
         targets: '.' + S.text + ' .letra',
         opacity: [0,1],
@@ -23,33 +23,33 @@ export const PageAboutMe = () => {
   return (
     <>
           <div className={S.container}>
-        <div className={S.description}>
-          <h6 className={S.text}>
-            <span>/*</span> <br/> <br/>
-            Sou graduanda em Sistemas de Informação pela Universidade Federal de
-            Alagoas e uma desenvolvedora front-end dedicada. Possuo uma base sólida
-            em JavaScript, ReactJs, React Native, HTML, CSS, Bootstrap e Sass, estou
-            constantemente buscando expandir meus conhecimentos e habilidades.
-          </h6>
-          <h6 className={S.text}>
-            Além dos meus estudos formais, sou uma entusiasta de projetos pessoais e
-            acadêmicos, desenvolvendo desde pequenas aplicações web até sistemas
-            complexos. Minha experiência abrange tanto projetos acadêmicos, quanto trabalhos freelancers,
-            onde aprendi a adaptar minhas habilidades às necessidades específicas do
-            cliente, valorizando a comunicação e colaboração.
-          </h6>
-          <h6 className={S.text}>
-            Estou comprometida com o aprendizado contínuo e o aprimoramento das
-            minhas habilidades como desenvolvedora. Além de buscar constantemente
-            novos conhecimentos e técnicas em cursos online, workshops e eventos da
-            área, também faço parte de comunidades de desenvolvedores, onde posso
-            compartilhar experiências, aprender com outros profissionais e
-            contribuir para o crescimento coletivo.
-            <br/>
-            <br />
-            <span>/*</span>
-          </h6>
-        </div>
+            <div className={S.description}>
+              <h6 className={S.text}>
+                <span>/*</span> <br/> <br/>
+                Sou graduanda em Sistemas de Informação pela Universidade Federal de
+                Alagoas e uma desenvolvedora front-end dedicada. Possuo uma base sólida
+                em JavaScript, ReactJs, React Native, HTML, CSS, Bootstrap e Sass, estou
+                constantemente buscando expandir meus conhecimentos e habilidades.
+              </h6>
+              <h6 className={S.text}>
+                Além dos meus estudos formais, sou uma entusiasta de projetos pessoais e
+                acadêmicos, desenvolvendo desde pequenas aplicações web até sistemas
+                complexos. Minha experiência abrange tanto projetos acadêmicos, quanto trabalhos freelancers,
+                onde aprendi a adaptar minhas habilidades às necessidades específicas do
+                cliente, valorizando a comunicação e colaboração.
+              </h6>
+              <h6 className={S.text}>
+                Estou comprometida com o aprendizado contínuo e o aprimoramento das
+                minhas habilidades como desenvolvedora. Além de buscar constantemente
+                novos conhecimentos e técnicas em cursos online, workshops e eventos da
+                área, também faço parte de comunidades de desenvolvedores, onde posso
+                compartilhar experiências, aprender com outros profissionais e
+                contribuir para o crescimento coletivo.
+                <br/>
+                <br />
+                <span>/*</span>
+              </h6>
+            </div>
       </div>
     </>
   );
